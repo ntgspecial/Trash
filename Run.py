5e1c328e147f4a9bec8534715a09876fcbcd51f1
ntgspecial <100434774+ntgspecial@users.noreply.github.com>
2023-02-07 01:12:35 +0530

    Create Run.py

---
+++ Run.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from CheckSMTPAccess import *
+import logging
+
+st.set_page_config(page_title='Check SMTP Access',page_icon=':wave:',layout='wide')
+st.title('Check SMTP Access') 
+
+Hosts = ["smtp-mail.outlook.com", "smtp.office365.com"]
+Ports = [587, 25, 465]
+
+for Host in Hosts:
+    for Port in Ports:
+        if CheckSmtpServer(Host, Port):
+            print("SMTP server is accessible With "+Host+':'+str(Port))
+            logging.info("SMTP server is accessible With "+Host+':'+str(Port))
+        else:
+            print("SMTP server is not accessible")
